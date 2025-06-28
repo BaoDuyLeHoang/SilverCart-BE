4464ebbbdd8840e2d33e071801d6841174949fa7
BaoDuyLeHoang <baoduylehoang2001@gmail.com>
2025-06-28 15:01:00 +0700

    add swagger to production

--- Apis/SilverCart.WebAPI/Program.cs
+++ Apis/SilverCart.WebAPI/Program.cs
@@ -61,7 +61,7 @@ var app = builder.Build();
 app.Services.GetService<IVnpay>()?.Initialize(configuration.Vnpay.TmnCode,
     configuration.Vnpay.HashSecret, configuration.Vnpay.BaseUrl, configuration.Vnpay.ReturnUrl);
 // Configure the HTTP request pipeline.
-if (IsDevelopment)
+if (true)
 {
     configuration.IsDevelopment = true;
     app.UseSwagger();
