edcd63822cc899bbcbb599f6a9c4b9a83272e869
NGUYEN_HA_LAM <lamnhse170579@fpt.edu.vn>
2025-08-11 22:14:44 +0700

    Fix error

---
+++ BEAPI/Controllers/ProductController.cs
@@ -0,0 +1,135 @@
+﻿using BEAPI.Dtos.Common;
+using BEAPI.Dtos.Product;
+using BEAPI.Services;
+using BEAPI.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BEAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Create([FromBody] ProductCreateDto dto)
+        {
+            try
+            {
+                await _service.Create(dto);
+                return Ok(new ResponseDto
+                {
+                    Message = "Product created successfully",
+                    Data = null
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseDto
+                {
+                    Message = ex.Message,
+                    Data = null
+                });
+            }
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await _service.GetAll();
+                return Ok(new ResponseDto
+                {
+                    Message = "Get all products successfully",
+                    Data = result
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseDto
+                {
+                    Message = ex.Message,
+                    Data = null
+                });
+            }
+        }
+
+        [HttpPut("[action]/{id}")]
+        public async Task<IActionResult> Update(string id, [FromBody] ProductCreateDto dto)
+        {
+            try
+            {
+                await _service.Update(dto, id);
+
+                return Ok(new ResponseDto
+                {
+                    Data = null,
+                    Message = "Product updated successfully"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseDto
+                {
+                    Data = null,
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("[action]/{productId}")]
+        public async Task<IActionResult> GetById(string productId)
+        {
+            try
+            {
+                var result = await _service.GetById(productId);
+                return Ok(new ResponseDto
+                {
+                    Message = "Get product successfully",
+                    Data = result
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseDto
+                {
+                    Message = ex.Message,
+                    Data = null
+                });
+            }
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Search([FromBody] ProductSearchDto dto)
+        {
+            try
+            {
+                var result = await _service.SearchAsync(dto);
+
+                return Ok(new ResponseDto
+                {
+                    Message = "Lấy danh sách sản phẩm thành công",
+                    Data = result
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseDto
+                {
+                    Message = ex.Message,
+                    Data = null
+                });
+            }
+        }
+    }
+
+}
